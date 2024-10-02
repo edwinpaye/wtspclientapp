83f9d953176632a492f970a8bd458ffa2d26dd77
edwinpaye <baratheonts@gmail.com>
2024-10-02 18:25:21 -0400

    express rate limited was added

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ const multer = require('multer');
 const mime = require('mime-types');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
+const rateLimit = require('express-rate-limit');
+
+// Rate limit for message sending (5 requests per minute)
+const sendMessageLimiter = rateLimit({
+    windowMs: 60 * 1000,  // 1 minute
+    max: 5,  // Limit each IP to 5 requests per windowMs
+    message: 'Demaciados mensajes enviados, espere un momento.'
+});
